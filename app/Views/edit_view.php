ef747e581a8cb68aaa24745a57cca351a7fcbfa5
AnthonyG21 <anthonyfreakingoncalves05@gmail.com>
2023-06-13 00:36:33 -0400

    roles,crud,tu mama, tiene to

---
+++ app/Views/edit_view.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>CRUD</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+  <style>
+    .error {
+      display: block;
+      padding-top: 5px;
+      font-size: 14px;
+      color: red;
+    }
+  </style>
+
+</head>
+<body>
+<div class="container">
+    <p><h1>Editar usuarios</h1></p>
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-8">
+        <form method="post" id="update_user" name="update_user" action="<?= site_url('/update') ?>">
+    <input type="hidden" name="id_usuarios" id="id" value="<?php echo $user_obj['id_usuarios']; ?>">
+
+    <div class="form-group">
+        <label>Nombre</label>
+        <input type="text" name="nombre" class="form-control" value="<?php echo $user_obj['nombre']; ?>">
+    </div>
+    <div class="form-group">
+        <label>Apellido</label>
+        <input type="text" name="apellido" class="form-control" value="<?php echo $user_obj['apellido']; ?>">
+    </div>
+    <div class="form-group">
+        <label>Cédula</label>
+        <input type="text" name="cedula" class="form-control" value="<?php echo $user_obj['cedula']; ?>">
+    </div>
+    <div class="form-group">
+        <label>Usuario</label>
+        <input type="text" name="usuario" class="form-control" value="<?php echo $user_obj['usuario']; ?>">
+    </div>
+    <div class="form-group">
+        <label>Teléfono</label>
+        <input type="text" name="telefono" class="form-control" value="<?php echo $user_obj['telefono']; ?>">
+    </div>
+    <div class="form-group">
+    <label>Rol</label>
+    <select class="form-select select-sm" name="roles" aria-label="Default select example">
+        <option selected>Selecciona un rol</option>
+        <?php foreach ($roles as $value => $label) { ?>
+            <option value="<?php echo $value; ?>" <?php echo ($user_obj['roles'] == $value) ? 'selected' : ''; ?>>
+                <?php echo $label; ?>
+            </option>
+        <?php } ?>
+    </select>
+</div>
+
+
+
+    <div class="form-group">
+        <button type="submit" class="btn btn-info btn-block">Guardar Datos</button>
+    </div>
+</form>
+
+        </div>
+        <div class="col-2"></div>
+    </div>
+</div>
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/additional-methods.min.js"></script>
+<script>
+    if ($("#update_user").length > 0) {
+      $("#update_user").validate({
+            'nombre': {
+            rules: 'required',
+            errors: {
+                required: 'Se necesita tu nombre.'
+            }
+            },
+
+            'apellido': {
+            rules: 'required',
+            errors: {
+                required: 'Se necesita tu apellido.'
+            }
+            },
+
+            'cedula': {
+            rules: 'required|min_length[7]|max_length[8]|is_unique[usuarios.cedula]|numeric',
+            errors: {
+                is_unique: 'Esta cédula ya ha sido elegida.',
+                numeric: 'La cédula debe ser un número.',
+                min_length: 'El campo cédula debe tener al menos 7 caracteres.'
+            }
+            },
+
+            'usuario': {
+            rules: 'required|is_unique[usuarios.usuario]|regex_match[/^' + $_POST['cedula'] + '.+$/]',
+            errors: {
+                required: 'Se necesita tu usuario.',
+                is_unique: 'Este usuario ya ha sido elegido.',
+                regex_match: 'El usuario debe comenzar con la cédula.'
+            }
+            },
+
+            'telefono': {
+            rules: 'required|exact_length[11]|is_unique[usuarios.telefono]',
+            errors: {
+                exact_length: 'El número de teléfono debe tener 11 dígitos',
+                is_unique: 'Este número de teléfono ya ha sido elegido.'
+            }
+            }
+
+      })
+    }
+  </script>
+</body>
+</html>
---
+++ app/Views/post-card.php
@@ -0,0 +1,56 @@
+
+<!-- Cartas de publicacion para el inicio -->
+<style>
+
+    /*Estilos del fondo de las cartas*/
+    .card {
+        background-color:#ffff;
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.3); /* Agregar sombra */
+    }
+
+    .btn {
+      background-color: #E6DA50;
+      color: rgb(255, 255, 255);
+      margin-left: auto;
+    }
+    
+    .btn:hover {
+      background-color: #7591FA;
+      color: black;
+    }
+
+    .pic {
+      border-radius: 10px;
+      margin-top: 2%;
+      max-width: 100%;
+      max-height: 300px;
+      object-fit: contain;
+      background-color: #fcf6b7;
+    }
+
+    .card-title {
+      color: #7591FA;
+    }
+
+</style>
+
+<div class="card " style="width: 18rem;">
+  <img src=<?php echo base_url('public/img/raton.webp'); ?> class="card-img-top pic" alt="...">
+
+  <div class="card-body">
+
+    <h4 class="card-title">Raton el misu</h4>
+
+    <p class="card-text">
+      gratiman
+      <i class="fa-solid fa-dollar-sign fa-xl"></i>
+    </p>
+
+    <div class="card-btn text-end">
+      <button type="button" class="btn btn-primary boton" href="#"">Ver</button>
+    </div>
+
+  </div>
+
+</div>
+
